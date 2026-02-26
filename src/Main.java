f06cd7997db6211346df498aff5ce7fa117f6555
= <=>
2026-02-26 14:44:44 +0400

    added GUI and the calculator code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import javax.swing.JOptionPane;
+
 public class Main{
     public static void main(String [] args){
-        System.out.println("Hello, world!");
+
+        double num1;
+        double num2;
+        char operator;
+        double result;
+        
+        num1 = Double.parseDouble(JOptionPane.showInputDialog("Enter the first number: "));
+        num2 = Double.parseDouble(JOptionPane.showInputDialog("Enter the second number: "));
+        operator = JOptionPane.showInputDialog("Enter the operator (+, -, *, /): ").charAt(0);
+
+        switch(operator){
+            case '+' -> {
+                result = num1 + num2;
+                JOptionPane.showMessageDialog(null, "The result is " + result);
+            }
+            case '-' -> {
+                result = num1 - num2;
+                JOptionPane.showMessageDialog(null, "The result is " + result);
+            }
+            case '*' -> {
+                result = num1 * num2;
+                JOptionPane.showMessageDialog(null, "The result is " + result);
+            }
+            case '/' -> {
+                result = num1 / num2;
+                JOptionPane.showMessageDialog(null, "The result is " + result);
+            }
+            default -> {
+                JOptionPane.showMessageDialog(null, "Invalid Operator!");
+            }
+        }
     }
-}
+}    
